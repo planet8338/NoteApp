8ddcb0d06bbf9ea216b30b21466165ab4b739fe5
abes <sbowkun@gmail.com>
2020-05-10 16:54:14 +0200

    Added SplashScreen, LoginActivity, RegisterActivity with xml.

--- app/src/main/java/com/sb/noteapp/v1/Activity/DashboardActivity.java
+++ app/src/main/java/com/sb/noteapp/v1/Activity/DashboardActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 import com.sb.noteapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
