829ab96159050b04dc3e1a4e91e7a5afa67975ca
sbowk <sbowkun@gmail.com>
2020-05-15 20:53:45 +0200

    Design/UI

--- app/src/main/java/com/sb/noteapp/v1/Activity/LoginActivity.java
+++ app/src/main/java/com/sb/noteapp/v1/Activity/LoginActivity.java
@@ -6,17 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.sb.noteapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button btnLogin, btnRegister;
+    EditText etLogin, etPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etLogin = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
 
         btnLogin = (Button) findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/sb/noteapp/v1/Activity/RegisterActivity.java
+++ app/src/main/java/com/sb/noteapp/v1/Activity/RegisterActivity.java
@@ -3,14 +3,33 @@ package com.sb.noteapp.v1.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.sb.noteapp.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText etName, etEmail, etPassword;
+    Button btnRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(RegisterActivity.this, "Register", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
